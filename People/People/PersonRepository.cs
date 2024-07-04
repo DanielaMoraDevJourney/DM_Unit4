11719d63e8c1dcd550f03155f1c0dd9a1d9c737f
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-07-03 21:13:50 -0500

    Modificación de métodos. ASYNC

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,67 +1,65 @@
 ﻿using People.Models;
 using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace People;
-
-public class PersonRepository
+namespace People
 {
-    string _dbPath;
-
-    public string StatusMessage { get; set; }
-
-
-    private SQLiteConnection conn;
-    private void Init()
+    public class PersonRepository
     {
-        if (conn != null)
-            return;
+        string _dbPath;
 
-        conn = new SQLiteConnection(_dbPath);
-        conn.CreateTable<Person>();
-    }
+        public string StatusMessage { get; set; }
 
-    public PersonRepository(string dbPath)
-    {
-        _dbPath = dbPath;                        
-    }
+        private SQLiteAsyncConnection conn;
 
-    public void AddNewPerson(string name)
-    {            
-        int result = 0;
-        try
+        private async Task Init()
         {
-            Init();
+            if (conn != null)
+                return;
 
-            // basic validation to ensure a name was entered
-            if (string.IsNullOrEmpty(name))
-                throw new Exception("Valid name required");
-
-            // TODO: Insert the new person into the database
-            //result = 0;
-            result = conn.Insert(new Person { Name = name });
-
-
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
-        }
-        catch (Exception ex)
-        {
-            StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            conn = new SQLiteAsyncConnection(_dbPath);
+            await conn.CreateTableAsync<Person>();
         }
 
-    }
-
-    public List<Person> GetAllPeople()
-    {
-        try
+        public PersonRepository(string dbPath)
         {
-            Init();
-            return conn.Table<Person>().ToList();
-        }
-        catch (Exception ex)
-        {
-            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            _dbPath = dbPath;
         }
 
-        return new List<Person>();
+        public async Task AddNewPerson(string name)
+        {
+            int result = 0;
+            try
+            {
+                await Init();
+
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("Valid name required");
+
+                result = await conn.InsertAsync(new Person { Name = name });
+
+                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            }
+        }
+
+        public async Task<List<Person>> GetAllPeople()
+        {
+            try
+            {
+                await Init();
+                return await conn.Table<Person>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return new List<Person>();
+        }
     }
 }
